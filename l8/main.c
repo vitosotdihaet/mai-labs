6914b44a898121a48f9d125172dbc4a737755d3b
vitosotdihaet <vitalikklimenko96@gmail.com>
2022-10-16 01:45:33 +0300

    ?

---
+++ l8/main.c
@@ -0,0 +1,12 @@
+/* Лабораторная работа 8, вариант n.
+ * Студент гр. М8О-103Б-22 Клименко В. М. */
+#include <stdio.h>
+
+const amongus = 100;
+
+int main() {
+	short int a;
+	a = printf("Hello world!\n");
+	printf("%d\n", amongus);
+	return 0;
+}
