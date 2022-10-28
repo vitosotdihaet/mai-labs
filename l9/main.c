335354ac259df790a6c11c357e994c4ded402a28
vitosotdihaet <vitostikitos@gmail.com>
2022-10-28 22:32:03 +0300

    lab9 upd

--- l9/main.c
+++ l9/main.c
@@ -2,7 +2,7 @@
  * Студент гр. М8О-103Б-22 Клименко В. М. */
 #include<stdio.h>
 
-short calc(long double i, long double j) {
+short calc(long long i, long long j) {
     if ((i + 10)*(i + 10) + (j + 10)*(j + 10) <= 100
     &&  (i + 20)*(i + 20) + (j + 20)*(j + 20) <= 100) {
         return 1;
@@ -10,32 +10,30 @@ short calc(long double i, long double j) {
     return 0;
 }
 
-long double max(long double a, long double b) {
+long long max(long long a, long long b) {
     if (a > b) { return a; }
     return b;
 }
 
-long double min(long double a, long double b) {
+long long min(long long a, long long b) {
     if (a < b) { return a; }
     return b;
 }
 
-long double abs(long double a) {
+long long abs(long long a) {
     if (a < 0) { return -a; }
     return a;
 }
 
-long double sign(long double a) {
+long long sign(long long a) {
     if (a >  0) { return  1; }
     if (a == 0) { return  0; } 
                   return -1;
 }
 
-long double mod(long double a, long double b) {
-    long long na = a;
-    long long nb = b;
+long long mod(long long a, long long b) {
     // printf("%Lf -> %lld; %Lf -> %lld\n", a, na, b, nb);
-    return na - na / nb * nb;
+    return a - a / b * b;
 }
 
 int main() {
@@ -43,7 +41,7 @@ int main() {
     // FILE *fp;
     // fp = fopen("for_graphics.txt", "a");
 
-    long double i[51] = {13.}, j[51] = {-9.}, l[51] = {-4.}, ii, jj, ll;
+    long long i[51] = {13}, j[51] = {-9}, l[51] = {-4}, ii, jj, ll;
     int kk;
     short gg = 0;
 
@@ -51,20 +49,20 @@ int main() {
     //     printf("%Lf ", l[x]);
     // }
 
-    for (int k = 1; k <= 50; ++k) {
+    for (int k = 1; k < 50; ++k) {
         kk = k - 1;
         
         ii = i[kk];
         jj = j[kk];
         ll = l[kk];
 
-        i[k] = mod(ii + jj, 30.0) / (mod(abs(ll), 5.0) + 1.0) + mod(ii + ll, 30.0)/(mod(abs(jj), 5.0) + 1.0);
-        j[k] = mod(max(kk * ii, (kk + 1.0) * jj), 25.0) - abs(jj - ll)/10.0;
-        l[k] = abs(jj - ll)/10.0 + min(mod(ii + ll, 20.0), mod(jj * kk, 20.0)) - 10.0;
-        // fprintf(fp, "k = %d:\ni = %Lf\nj = %Lf\nl = %Lf\n", k, i[k], j[k], l[k]);
+        i[k] = mod(ii + jj, 30) / (mod(abs(ll), 5) + 1) + mod(ii + ll, 30)/(mod(abs(jj), 5) + 1);
+        j[k] = mod(max(kk * ii, (kk + 1) * jj), 25) - abs(jj - ll)/10;
+        l[k] = abs(jj - ll)/10 + min(mod(ii + ll, 20), mod(jj * kk, 20)) - 10;
+        // fprintf(fp, "k = %d:\ni = %lld\nj = %lld\nl = %lld\n", k, i[k], j[k], l[k]);
         
         if (calc(i[k], j[k]) == 1) {
-            printf("Point is in at k = %d!\ni = %Lf\nj = %Lf\nl = %Lf\n", k, i[k], j[k], l[k]);
+            printf("Point is in at k = %d!\ni = %lld\nj = %lld\nl = %lld\n", k, i[k], j[k], l[k]);
             gg = 1;
             break;
         }
