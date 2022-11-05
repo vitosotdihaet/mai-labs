71e1490f1c12901b84d8bd3b3fd7d61766da3870
Vitaly Klimenko <vitostikitos@gmail.com>
2022-11-05 12:07:12 +0300

    lab9 update

--- l9/main.c
+++ l9/main.c
@@ -1,8 +1,10 @@
 /* Лабораторная работа №9, вариант №13
  * Студент гр. М8О-103Б-22 Клименко В. М. */
-#include<stdio.h>
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
 
-short calc(long long i, long long j) {
+short calc(int i, int j) {
     if ((i + 10)*(i + 10) + (j + 10)*(j + 10) <= 100
     &&  (i + 20)*(i + 20) + (j + 20)*(j + 20) <= 100) {
         return 1;
@@ -10,37 +12,25 @@ short calc(long long i, long long j) {
     return 0;
 }
 
-long long max(long long a, long long b) {
+int max(int a, int b) {
     if (a > b) { return a; }
     return b;
 }
 
-long long min(long long a, long long b) {
+int min(int a, int b) {
     if (a < b) { return a; }
     return b;
 }
 
-long long abs(long long a) {
-    if (a < 0) { return -a; }
-    return a;
-}
-
-long long sign(long long a) {
+int sign(int a) {
     if (a >  0) { return  1; }
     if (a == 0) { return  0; } 
                   return -1;
 }
 
-long long mod(long long a, long long b) {
-    long long fl = floor((double) a / (double) b); 
+int mod(int a, int b) {
+    int fl = floor((double) a / (double) b);
     return a - fl * b;
-}
-
-long long floor(double x) {
-    if (x - (long long) x >= 0) {
-        return (long long) x;
-    }
-    return (long long) x - 1;
 }
 
 int main() {
@@ -48,24 +38,22 @@ int main() {
     FILE *fp;
     fp = fopen("for_graphics.txt", "w");
 
-    long long ip = 13, jp = -9, lp = -4, ii, jj, ll;
-    int kk;
+    int ik = 13, jk = -9, lk = -4, ii, jj, ll, kk;
     short gg = 0;
 
     for (int k = 1; k < 50; ++k) {
         kk = k - 1;
-        
-        ii = ip;
-        jj = jp;
-        ll = lp;
+        ii = ik;
+        jj = jk;
+        ll = lk;
 
-        ip = mod(ii + jj, 30) / (mod(abs(ll), 5) + 1) + mod(ii + ll, 30)/(mod(abs(jj), 5) + 1);
-        jp = mod(max(kk * ii, (kk + 1) * jj), 25) - abs(jj - ll)/10;
-        lp = abs(jj - ll)/10 + min(mod(ii + ll, 20), mod(jj * kk, 20)) - 10;
-        fprintf(fp, "k = %d:\ni = %lld\nj = %lld\nl = %lld\n", k, ip, jp, lp);
+        ik = mod(ii + jj, 30) / (mod(abs(ll), 5) + 1) + mod(ii + ll, 30)/(mod(abs(jj), 5) + 1);
+        jk = mod(max(kk * ii, (kk + 1) * jj), 25) - abs(jj - ll)/10;
+        lk = abs(jj - ll)/10 + min(mod(ii + ll, 20), mod(jj * kk, 20)) - 10;
+        fprintf(fp, "k = %d:\ni = %d\nj = %d\nl = %d\n", k, ik, jk, lk);
         
-        if (calc(ip, jp) == 1) {
-            printf("Point is in at k = %d!\ni = %lld\nj = %lld\nl = %lld\n", k, ip, jp, lp);
+        if (calc(ik, jk) == 1) {
+            printf("Point is in at k = %d!\ni = %d\nj = %d\nl = %d\n", k, ik, jk, lk);
             gg = 1;
             break;
         }
@@ -73,6 +61,7 @@ int main() {
 
     if (gg == 0) {
         printf("Couldn't make it in 50 iterations! :(\n");
+        printf("Last point:\ni = %d\nj = %d\nl = %d\n", ik, jk, lk);
     }
 
     fclose(fp);
