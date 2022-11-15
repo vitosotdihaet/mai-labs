f490cef94c5f8fd5176955272790e07d9ba0d875
vitosotdihaet <vitostikitos@gmail.com>
2022-11-15 21:41:30 +0300

    Rewrite output

--- l11/main.c
+++ l11/main.c
@@ -15,9 +15,9 @@ int main() {
         END = read_string(&num);
         if (END) break;
         if (atoll(num.values) <= __INT_MAX__) {
-            printf("This number is lower than INT_MAX! Remain %s!\n", num.values);
+            printf("This number is less than INT_MAX! Remain %s!\n", num.values);
         } else {
-            printf("This number is higher than INT_MAX! Delete!\n");
+            printf("This number is greater than INT_MAX! Delete!\n");
         }
     }
     printf("End of programm...\n");
