eee5d6f5226139c9d4ff063ce619221b130b7daf
vitosotdihaet <vitostikitos@gmail.com>
2022-11-19 13:31:47 +0300

    savim

--- l12/main.c
+++ l12/main.c
@@ -1,36 +1,66 @@
 /* Лабораторная работа №12 вариант №30    */
 /* Студент гр. М8О-103Б-22 Клименко В. М. */
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include "string.h"
+#include <ctype.h>
+#include <math.h>
 
-void solve(string* s) {
-    char t1, t2;
-    reverse_string(s);
-    for (unsigned long i = 0; i < s->last_element - s->last_element % 2; i += 2) {
-        t1 = s->values[i];
-        t2 = s->values[i + 1];
-        if (t2 == '-') continue;
-        if (t1 - '0' < t2 - '0') {
-            s->values[i] = t2;
-            s->values[i + 1] = t1;
-        } 
+long long answ(char s[], long long l) {
+    char *_s = (char*) calloc(l, l * sizeof(char));
+    // printf("%s\n", s);
+    for (long long i = 0; i < l; ++i) {
+        _s[i] = s[l - i - 1];
     }
-    reverse_string(s);
+    // printf("%s\n", _s);
+    return atoll(_s);
+}
+
+unsigned long long mpow(unsigned long long a, unsigned long long n) {
+    unsigned long long exp = 1;
+    for (unsigned long long i = 1; i <= n; ++i) {
+        exp *= a;
+    }
+    return exp;
+}
+
+long long solve(long long s) {
+    long long l = (long long) log10((double) abs(s)) + 1;
+    long long ns = 0;
+    if (s < 0) {
+        printf("-");
+        s = -s;
+    }
+    for (long long i = l - 1; i > -1; --i) {
+        // printf("at i = %lld: %d - %c\n", i + off, (s / mpow(10, i)) % 10, (s / mpow(10, i)) % 10 + '0');
+        str[i] = (s / mpow(10, i)) % 10 + '0';
+        // printf("%c\n", t1[l - i - 1]);
+    }
+    // printf("%s\n", str);
+
+    // for (long long i = 0; i < l - l % 2; i += 2) {
+    //     t1 = ns / mpow(10, i);
+    //     t2 = str[i + 1];
+    //     if (t2 == '-') continue;
+    //     if (t1 < t2) {
+    //         str[i] = t2;
+    //         str[i + 1] = t1;
+    //     } 
+    // }
+    return 
 }
 
 int main() {
     int END = 0;
-    string inp;
-
-    init_string(&inp);
+    long long inp = 0;
+    long long out = 0;
 
     while (1) {
-        init_string(&inp);
-        END = read_string(&inp);
-        if (END) break;
-        solve(&inp);
-        printf("Number after processing: %s\n", inp.values);
+        END = scanf("%lld", &inp);
+        if (END == -1) break;
+        out = solve(inp);
+        printf("%lld\n", out);
+
     }
 
     printf("End of programm...\n");
