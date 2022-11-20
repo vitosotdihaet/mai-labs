dcf9d2a7fff202fe85b2c27caf7599b971e27a28
vitosotdihaet <vitostikitos@gmail.com>
2022-11-20 19:45:44 +0300

    yay

--- l12/main.c
+++ l12/main.c
@@ -2,19 +2,7 @@
 /* Студент гр. М8О-103Б-22 Клименко В. М. */
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
 #include <math.h>
-
-long long answ(char s[], long long l) {
-    char *_s = (char*) calloc(l, l * sizeof(char));
-    // printf("%s\n", s);
-    for (long long i = 0; i < l; ++i) {
-        _s[i] = s[l - i - 1];
-    }
-    // printf("%s\n", _s);
-    return atoll(_s);
-}
 
 unsigned long long mpow(unsigned long long a, unsigned long long n) {
     unsigned long long exp = 1;
@@ -24,30 +12,29 @@ unsigned long long mpow(unsigned long long a, unsigned long long n) {
     return exp;
 }
 
-long long solve(long long s) {
-    long long l = (long long) log10((double) abs(s)) + 1;
-    long long ns = 0;
-    if (s < 0) {
-        printf("-");
-        s = -s;
+long long solve(long long n) {
+    // long long answ = 0;
+    long long l = (long long) log10((double) abs(n)) + 1;
+    int n1, n2;
+    short otr = -1;
+    if (n < 0) {
+        n = -n;
+        otr = 1;
     }
-    for (long long i = l - 1; i > -1; --i) {
-        // printf("at i = %lld: %d - %c\n", i + off, (s / mpow(10, i)) % 10, (s / mpow(10, i)) % 10 + '0');
-        str[i] = (s / mpow(10, i)) % 10 + '0';
-        // printf("%c\n", t1[l - i - 1]);
-    }
-    // printf("%s\n", str);
 
-    // for (long long i = 0; i < l - l % 2; i += 2) {
-    //     t1 = ns / mpow(10, i);
-    //     t2 = str[i + 1];
-    //     if (t2 == '-') continue;
-    //     if (t1 < t2) {
-    //         str[i] = t2;
-    //         str[i + 1] = t1;
-    //     } 
-    // }
-    return 
+    for (int i = 0; i < l - 1; i += 2) {
+        n1 = n / mpow(10, i) % 10;
+        n2 = n / mpow(10, i + 1) % 10;
+        printf("i = %d:\nn1 = %d, n2 = %d\n", i, n1, n2);
+        if (n1 < n2) {
+            n -= n1 * mpow(10, i);
+            n -= n2 * mpow(10, i + 1);
+            n += n2 * mpow(10, i);
+            n += n1 * mpow(10, i + 1);
+        }
+    }
+
+    return n * -1 * otr;
 }
 
 int main() {
