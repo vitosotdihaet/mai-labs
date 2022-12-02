1534b4d9d6a8006d08a59dc850e70411e6ce4a5f
vitosotdihaet <vitostikitos@gmail.com>
2022-12-03 00:04:07 +0300

    CW4 upd

--- c4/main.c
+++ c4/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <math.h>
 
+#include<unistd.h>
+
 long double mabs(long double a) {
     if (a < 0) {
         return -a;
@@ -12,14 +14,20 @@ long double f1(long double x) {
     return pow(M_E, x) + sqrt(1 + pow(M_E, 2*x)) - 2;
 }
 
+long double fp1(long double x) {
+    return pow(M_E, x) + 1/(sqrt(1 + pow(M_E, 2*x)));
+}
+
 long double f2(long double x) {
-    return log(x)/log(M_E) - x + 1.8;
+    return log(x) - x + 1.8;
+}
+
+long double fp2(long double x) {
+    return 1/x - 1;
 }
 
 long double dichotomy(long double a, long double b, long double e, long double (*f) (long double)) {
     long double c = (a + b) / 2., fa = f(a), fb = f(b), fc = f(c);
-    // printf("%.10Lf, %.10Lf, %.10Lf\n", a, b, c);
-    // printf("%.50Lf <? %.50Lf\n", mabs(fa - fb), e);
     long double answ;
     if (mabs(fa - fb) < e) {
         answ = c;
@@ -31,6 +39,38 @@ long double dichotomy(long double a, long double b, long double e, long double (
         answ = c;
     }
     return answ;
+}
+
+long double iterations1(long double x, long double e) {
+    long double fx = log(2 - sqrt(1 + pow(M_E, 2*x)));
+    if (mabs(x - fx) < e) {
+        return x;
+    }
+    return iterations1(fx, e);
+}
+
+long double iterations2(long double x, long double e) {
+    long double fx = log(x) + 1.8;
+    if (mabs(x - fx) < e) {
+        return x;
+    }
+    return iterations2(fx, e);
+}
+
+long double newton1(long double x, long double e) {
+    long double fx = x - f1(x)/fp1(x);
+    if (mabs(x - fx) < e) {
+        return fx;
+    }
+    return newton1(fx, e);
+}
+
+long double newton2(long double x, long double e) {
+    long double fx = x - f2(x)/fp2(x);
+    if (mabs(x - fx) < e) {
+        return fx;
+    }
+    return newton2(fx, e);
 }
 
 long double calc_eps() {
@@ -48,11 +88,14 @@ int main() {
     long double a1 = -1., b1 = 0.;
     long double a2 = 2., b2 = 3.;
 
-    printf("______________________________________________________________\n");
-    printf("Function:                 | Methods:   | Root:               \n");
-    printf("e^x + sqrt(1 + e^(2x) - 2 | Dichotomy: | %.20Lf\n", dichotomy(a1, b1, k * eps, f1));
-    printf("lnx - x + 1.8             | Dichotomy: | %.20Lf\n", dichotomy(a2, b2, k * eps, f2));
-
+    printf("_________________________________________________________________\n");
+    printf("Function:                  | Methods:   | Root:                  \n");
+    printf("e^x + sqrt(1 + e^(2x)) - 2 | Dichotomy  | %.20Lf\n", dichotomy(a1, b1, k * eps, f1));
+    printf("e^x + sqrt(1 + e^(2x)) - 2 | Iterations | %.20Lf\n", iterations1((a1 + b1)/2., k * eps * 10000.));
+    printf("e^x + sqrt(1 + e^(2x)) - 2 | Newton     | %.20Lf\n", newton1((a1 + b1)/2., k * eps * 10.));
+    printf("lnx - x + 1.8              | Dichotomy  | %.20Lf\n", dichotomy(a2, b2, k * eps, f2));
+    printf("lnx - x + 1.8              | Iterations | %.20Lf\n", iterations2((a2 + b2)/2., k * eps * 10000.));
+    printf("lnx - x + 1.8              | Newton     | %.20Lf\n", newton2((a2 + b2)/2., k * eps * 10.));
 
     return 0;
 }
