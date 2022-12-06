9bfa736947f53b78db4eefaaf8958a85920a63c2
vitosotdihaet <vitostikitos@gmail.com>
2022-12-06 23:43:41 +0300

    CW4 upd

--- c4/main.c
+++ c4/main.c
@@ -11,24 +11,32 @@ long double mabs(long double a) {
 }
 
 long double f1(long double x) {
-    return pow(M_E, x) + sqrt(1 + pow(M_E, 2*x)) - 2;
+    return powl(M_E, x) + sqrtl(1 + powl(M_E, 2*x)) - 2;
 }
 
 long double fd1(long double x) {
-    return pow(M_E, x) + 1/(sqrt(1 + pow(M_E, 2*x)));
+    return powl(M_E, x) + 1/(sqrtl(1 + powl(M_E, 2*x)));
+}
+
+long double fn1(long double x) {
+    return logl(2 - sqrtl(1 + powl(M_E, 2*x)));
 }
 
 
 long double f2(long double x) {
-    return log(x) - x + 1.8;
+    return logl(x) - x + 1.8;
 }
 
 long double fd2(long double x) {
     return 1/x - 1;
 }
 
+long double fn2(long double x) {
+    return logl(x) + 1.8;
+}
+
 long double dichotomy(long double a, long double b, long double e, long double (*f) (long double)) {
-    long double c = (a + b) / 2., fa = f(a), fb = f(b), fc = f(c);
+    long double c = (a + b) / 2., fb = f(b), fc = f(c);
     long double answ;
     if (c + e > b) { // same as x ≈ (ak + bk)/2
         answ = c;
@@ -43,37 +51,20 @@ long double dichotomy(long double a, long double b, long double e, long double (
 }
 
 
-long double iterations1(long double x, long double e) {
-    long double fx = log(2 - sqrt(1 + pow(M_E, 2*x)));
+long double iterations(long double x, long double e, long double (*f) (long double), long double (*fn) (long double)) {
+    long double fx = fn(x);
     if (mabs(x - fx) < e) {
         return x;
     }
-    return iterations1(fx, e);
+    return iterations(fx, e, f, fn);
 }
 
-long double iterations2(long double x, long double e) {
-    long double fx = log(x) + 1.8;
-    if (mabs(x - fx) < e) {
-        return x;
-    }
-    return iterations2(fx, e);
-}
-
-
-long double newton1(long double x, long double e) {
-    long double fx = x - f1(x)/fd1(x);
+long double newton(long double x, long double e, long double (*f) (long double), long double (*fd) (long double)) {
+    long double fx = x - f(x)/fd(x);
     if (mabs(x - fx) < e) {
         return fx;
     }
-    return newton1(fx, e);
-}
-
-long double newton2(long double x, long double e) {
-    long double fx = x - f2(x)/fd2(x);
-    if (mabs(x - fx) < e) {
-        return fx;
-    }
-    return newton2(fx, e);
+    return newton(fx, e, f, fd);
 }
 
 
@@ -94,11 +85,11 @@ int main() {
     long double a2 = 2., b2 = 3.;
 
     long double d1 = dichotomy(a1, b1, k * eps, f1), d2 = dichotomy(a2, b2, k * eps, f2);
-    long double i1 = iterations1((a1 + b1)/2., k * eps * 10000.), i2 = iterations2((a2 + b2)/2., k * eps);
-    long double n1 = newton1((a1 + b1)/2., k * eps), n2 = newton2((a2 + b2)/2., k * eps * 10.);
+    long double i1 = iterations((a1 + b1)/2., k * eps * 10000., f1, fn1), i2 = iterations((a2 + b2)/2., k * eps, f2, fn2);
+    long double n1 = newton((a1 + b1)/2., k * eps, f1, fd1), n2 = newton((a2 + b2)/2., k * eps * 10., f2, fd2);
 
     printf("___________________________________________________________________________________________\n");
-    printf("Function:                  | Method :   | Root:                   | Function at found x:   \n");
+    printf("Function:                  | Method:    | Root:                   | Function at found x:   \n");
     printf("e^x + sqrt(1 + e^(2x)) - 2 | Dichotomy  | %.20Lf | %.21Lf\n", d1, f1(d1));
     printf("e^x + sqrt(1 + e^(2x)) - 2 | Iterations | %.20Lf | %.21Lf\n", i1, f1(d1));
     printf("e^x + sqrt(1 + e^(2x)) - 2 | Newton     | %.20Lf | %.21Lf\n", n1, f1(d1));
