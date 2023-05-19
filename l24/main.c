9c6f2d7b92cc1edd33e99139f0b348a5b33707a5
vitosotdihaet <vitostikitos@gmail.com>
2023-05-20 00:54:41 +0300

    Gavno a ne laba, ya ne budu eto dodelyvat

--- l24/main.c
+++ l24/main.c
@@ -9,17 +9,6 @@
 int main() {
     Node root;
 
-    // char initial_expression[256] = {};
-    // printf("Input math expression: ");
-    // scanf("%s", &initial_expression);
-
-    // node_create_root(&root, "((1+(12)))- (bebra * 13)^(45/1)+among");
-    // node_create_root(&root, "((1+2))-((7) + 13)");
-    // node_create_root(&root, "1+2+3+4");
-    // node_create_root(&root, initial_expression);
-
-    // printf("DEBUG in main.c: %d\n", DEBUG);
-
     node_create_root(&root, "3*9 - 3 * 5 + 50");
     printf("Original:\n");
     node_build_tree(&root);
@@ -27,7 +16,6 @@ int main() {
     printf("\n");
 
     printf("Task:\n");
-    // node_task(&root);
     node_take_out_factors(root.left->left, root.left->right);
     node_print(root);
     printf("\n");
@@ -41,7 +29,6 @@ int main() {
     printf("\n");
 
     printf("Task:\n");
-    // node_task(&root);
     node_take_out_factors(root.left, root.right->right);
     node_print(root);
     printf("\n");
@@ -55,24 +42,25 @@ int main() {
     printf("\n");
 
     printf("Task:\n");
-    // node_task(&root);
     node_take_out_factors(root.left->left, root.left->right);
     node_print(root);
     printf("\n");
 
-    // printf("\n");
 
-    // node_create_root(&root, "3*9 + 50 - 3 * 5 + 13");
-    // printf("Original:\n");
-    // node_build_tree(&root);
-    // node_print(root);
-    // printf("\n");
+    char expr[256] = {};
+    printf("\nInput your math expression (without spaces): ");
+    scanf("%s", &expr);
 
-    // printf("Task:\n");
-    // // node_task(&root);
-    // node_take_out_factors(root.left, root.right->right);
-    // node_print(root);
-    // printf("\n");
+    node_create_root(&root, expr);
+    printf("Original:\n");
+    node_build_tree(&root);
+    node_print(root);
+    printf("\n");
+
+    printf("Task:\n");
+    node_take_out_factors(root.left, root.right);
+    node_print(root);
+    printf("\n");
 
     return 0;
 }
