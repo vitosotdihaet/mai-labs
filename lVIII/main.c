85c157a17fbd491464cd0f68600c3e991bc489c7
vitosotdihaet <vitostikitos@gmail.com>
2023-05-28 15:51:57 +0300

    Upd lVIII

--- lVIII/main.c
+++ lVIII/main.c
@@ -6,13 +6,40 @@
 int main() {
     linked_list *ll = ll_alloc();
 
-    ll_set_value(ll, "aboba");
-    ll_push_right(ll, "7");
-    ll_push_right(ll, "2");
-    ll_push_right(ll, "3");
-    ll_push_right(ll, "4");
-    ll_push_right(ll, "5");
+    ll_set_value(ll, "aaa");
+    
+    ll_push_right(ll, "b");
+    ll_push_right(ll, "c");
+    ll_push_right(ll, "eerh");
+    ll_push_right(ll, "d");
+    ll_push_right(ll, "faa");
+    printf("Initial list:\n");
     ll_print(ll);
+    
+    ll = ll_remove(ll->next);
+    printf("Without second element:\n");
+    ll_print(ll);
+
+    ll = ll_remove_left(ll);
+    printf("Without leftmost element:\n");
+    ll_print(ll);
+
+    if (ll_is_sorted(ll)) {
+        printf("Linked list is sorted!\n");
+    } else {
+        printf("Linked list is not sorted!\n");
+    }
+
+    ll = ll_remove_right(ll);
+    printf("Without rightmost element:\n");
+    ll_print(ll);
+
+    if (ll_is_sorted(ll)) {
+        printf("Linked list is sorted!\n");
+    } else {
+        printf("Linked list is not sorted!\n");
+    }
+
     ll_dealloc(ll);
 
     return 0;
